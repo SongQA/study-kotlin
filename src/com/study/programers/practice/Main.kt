f33d8653d0b8d6113b53ebbd3af0778385873c20
eck.song92 <eck.song92@rainist.com>
2023-03-25 17:55:37 +0900

    Add Programers

---
+++ src/com/study/programers/practice/Main.kt
@@ -0,0 +1,7 @@
+package com.study.programers.practice
+
+fun main() {
+//    println(약수의_합().solution(5))
+//    println(자릿수_더하기().solution(987))
+    println(x만큼_간격이_있는_n개의_숫자().solution(2,5).contentToString())
+}
